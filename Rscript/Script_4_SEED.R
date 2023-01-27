e52c6140079d9df50614f0721ef63a08bb47d8be
ibarrioh <ibarrioh@hotmail.com>
2023-01-27 09:07:48 +0000

    Create Script_4_SEED.R

---
+++ Rscript/Script_4_SEED.R
@@ -0,0 +1,24 @@
+
+####Jaccard score calculations (genes from significant modules, figures 3 & 4)
+
+jaccard.IB<-function(set1,set2){
+  jac=sum(set1%in%set2)/sum(!duplicated(c(set1,set2)))
+  return(jac)
+}
+
+######
+
+setwd("/tables_expansion")
+
+jaccard.KS=readRDS("/tables_expansion/jaccard_KS.rds")
+geneList=readRDS("/tables_expansion/genesList_KS.rds")
+
+for(i in 1:nrow(jaccard.KS)){
+  
+  jaccard.KS[i,"jaccIndx"]=jaccard.IB(unlist(geneList[jaccard.KS[i,"A"]]),
+                                      unlist(geneList[jaccard.KS[i,"B"]]))
+  
+}
+
+saveRDS(jaccard.KS,"/tables_expansion/jaccard_KS_results.rds")
+
